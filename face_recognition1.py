bf83f104089fec80dccfbf11d8dee8ea8c6066d8
varunrocxx <varungupta117@gmail.com>
2019-08-25 13:29:52 +0530

    Adding Face recognition

--- face_recognition1.py
+++ face_recognition1.py
@@ -1,7 +1,10 @@
-import argparse
+import numpy as np 
+from sklearn.neighbors import KNeighborsClassifier
 import cv2
+import os
 import imutils
-import numpy as np
+import argparse
+
 
 argp=argparse.ArgumentParser()
 argp.add_argument('-p','--protxt',required=True,help='Protxt file for model architecture')
@@ -10,16 +13,33 @@ argp.add_argument('-c','--confidence',default=0.5,help='confidence of detected f
 arguments=vars(argp.parse_args())
 
 
+
+knn = KNeighborsClassifier(n_neighbors=4)
+
+cap = cv2.VideoCapture(0)
+
+labels = []
+path = 'data/'
+name={}
+index = 0
+face_data = []
+
+for fx in os.listdir(path):
+
+	if fx.endswith('.npy'):
+		name[index] = fx[:-4]
+		file = np.load(path+fx,allow_pickle = True)
+		face_data.append(file)
+		labels.append(index*np.ones((file.shape[0],)))
+		index = index+1
+
+face_train = np.concatenate(face_data,axis = 0)
+labels = np.concatenate(labels,axis = 0).reshape((-1,1))
+train_data = np.concatenate((face_train,labels),axis =1)
+
+knn.fit(train_data[:,:-1],train_data[:,-1])
+
 cnet = cv2.dnn.readNetFromCaffe(arguments["protxt"], arguments["model"])
-
-#cnet=cv2.dnn.readNetFromCaffe(arguments['model'],arguments['protxt'])
-
-name = input('Enter your name : ')
-
-cap=cv2.VideoCapture(0)
-
-face_data =[]
-count = 0
 
 while True:
 
@@ -28,6 +48,7 @@ while True:
 		continue
 
 	#cv2.imshow('frame',frame)
+	
 	frame = cv2.flip(frame,1)
 	frame = imutils.resize(frame, width=400)
 
@@ -46,19 +67,15 @@ while True:
 		if confidence<arguments['confidence']:
 			continue
 
-		text = "{:.4f}%".format(confidence * 100)
+		#text = "{:.4f}%".format(confidence * 100)
 
 
 		box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
 		
 		(startX, startY, endX, endY) = box.astype("int")
-		offset=10
-		y=startY-offset
-		cv2.putText(frame, text, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 240, 255), 2)
 
+		
 
-		cv2.rectangle(frame,(startX,startY),(endX,endY),(255,255,255),2)
-		cv2.imshow('face',frame)
 		#print(startX,startY)
 		
 		if startY<0:
@@ -68,53 +85,34 @@ while True:
 
 		a = frame[ startY : endY , startX : endX ]
 		a = cv2.resize(a,(100,100))
-		count = count +1
-		if count % 2 == 0 :	
+		a = cv2.cvtColor(a, cv2.COLOR_RGB2GRAY)
+		a = np.array(a)
+		a = a.reshape(1,-1)
+		text = name[int(knn.predict(a))]
+		offset=10
+		y=startY-offset
+		cv2.putText(frame, text, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 240, 255), 2)
+		cv2.rectangle(frame,(startX,startY),(endX,endY),(25,102,205),3)
+		cv2.imshow('face',frame)
 
-			a = cv2.cvtColor(a, cv2.COLOR_RGB2GRAY)
-			face_data.append(a)
-		
-		print(count)
-		cv2.imshow('abc',a)
-		
 		#forcc = cv2.VideoWriter_fourcc(*'YUY2')
 		#video = cv2.VideoWriter('data/video.mp4',forcc,20.0,(640,480))
-	if count == 500:
-		break
+	
 
 	if cv2.waitKey(1) & 0xFF == ord('q') :
 		break
-
-# print('frame',frame.shape)
-# print('blob',blob.shape)
-# print(predictions.shape)
+	
 cap.release()
 cv2.destroyAllWindows()
 
 
 
-# Saving the file:
+
+#print(train_data[:,:-1])
+#print(train_data[:,-1])
 
 
-
-face_data=np.asarray(face_data)
-print(face_data.shape)
-face_data=face_data.reshape((face_data.shape[0],-1))
-np.save('data/'+ name+'.npy',face_data)
-print('data saved succesfully')
-
-
-#Code to run this programm
-# python face_detection.py --protxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
-
-
-
-
-
-
-
-#Notes by me:	
-# at a typical run the shapes are
-# frame (300, 400, 3)
-# blob (1, 3, 300, 300)
-# predictions(1, 1, 110, 7)
+#for i in range(1000):
+#	for j in range(1000):
+#		if train_data[i][j] != 255:
+#			print(train_data[i][j])
